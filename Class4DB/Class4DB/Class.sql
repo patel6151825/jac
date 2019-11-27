c6345a02ef494c97ec97b9f0e59c5d951f6bce65
Payal Patel <patelpayal1796@gmail.com>
2019-11-27 18:16:13 -0500

    created student and class table

---
+++ Class4DB/Class4DB/Class.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Class]
+(
+	[ClassId] TINYINT NOT NULL PRIMARY KEY IDENTITY, 
+    [Name] VARCHAR(20) NOT NULL, 
+    [No_Of_Students] INT NULL
+)
---
+++ Class4DB/Class4DB/Student.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Student]
+(
+	[StudentId] TINYINT NOT NULL PRIMARY KEY NONCLUSTERED IDENTITY, 
+    [FirstName] VARCHAR(50) NOT NULL, 
+    [LastName] VARCHAR(50) NOT NULL, 
+    [Email] VARCHAR(30) NULL, 
+    [Contact] NUMERIC(10) NOT NULL, 
+    [ClassId] INT NOT NULL, 
+    CONSTRAINT [FK_Student_ToTable] FOREIGN KEY ([Column]) REFERENCES [ToTable]([ToTableColumn])
+)
