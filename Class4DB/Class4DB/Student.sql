504f513851429e3a2990cd84b086a8efd5b5949f
Payal Patel <patelpayal1796@gmail.com>
2019-11-27 18:47:10 -0500

    Add two foreign keys in student_class table

--- Class4DB/Class4DB/Student.sql
+++ Class4DB/Class4DB/Student.sql
@@ -4,5 +4,5 @@
     [FirstName] VARCHAR(50) NOT NULL, 
     [LastName] VARCHAR(50) NOT NULL, 
     [Email] VARCHAR(30) NULL, 
-    [Age] INT NOT NULL
+    [Age] INT NOT NULL 
 )
--- Class4DB/Class4DB/Student_Class.sql
+++ Class4DB/Class4DB/Student_Class.sql
@@ -1,6 +1,8 @@
 ﻿CREATE TABLE [dbo].[Student_Class]
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [StudentId] INT NOT NULL, 
-    [ClassId] INT NOT NULL
+    [StudentId] TINYINT NOT NULL, 
+    [ClassId] TINYINT NOT NULL, 
+    CONSTRAINT [FK_Student_Class_Student] FOREIGN KEY ([StudentId]) REFERENCES [Student]([StudentId]), 
+    CONSTRAINT [FK_Student_Class_Class] FOREIGN KEY ([ClassId]) REFERENCES [Class]([ClassId])
 )
