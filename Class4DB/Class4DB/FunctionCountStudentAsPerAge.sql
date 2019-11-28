f6e5e851499b8e019fda36f045f8cb46a2da21d9
Payal Patel <patelpayal1796@gmail.com>
2019-11-28 17:41:04 -0500

    Created One Scalar Function

---
+++ Class4DB/Class4DB/FunctionCountStudentAsPerAge.sql
@@ -0,0 +1,14 @@
+﻿CREATE FUNCTION [dbo].[FunctionCountStudentAsPerAge]
+(
+	@param1 int
+)
+RETURNS INT
+AS
+BEGIN
+	DECLARE @totalStudents int;
+	SELECT @totalStudents=COUNT(StudentId) FROM Student S WHERE S.Age=@param1
+	IF (@totalStudents IS NULL)
+		SET @totalStudents=0;
+	RETURN @totalStudents
+END
+GO
