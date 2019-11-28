6bc224c8b378b365fa542a7329cc7f09cffcfa63
Payal Patel <patelpayal1796@gmail.com>
2019-11-28 18:41:05 -0500

    Stop entering data again and again in script

--- Class4DB/Class4DB/Script.PostDeployment1.sql
+++ Class4DB/Class4DB/Script.PostDeployment1.sql
@@ -9,21 +9,30 @@ Post-Deployment Script Template
                SELECT * FROM [$(TableName)]					
 --------------------------------------------------------------------------------------
 */
-INSERT INTO dbo.Student VALUES ('Firstname1','LastName1',23,GETDATE(),null)
-INSERT INTO dbo.Student VALUES ('Firstname2','LastName2',24,GETDATE(),null)
-INSERT INTO dbo.Student VALUES ('Firstname3','LastName3',25,GETDATE(),null)
-INSERT INTO dbo.Student VALUES ('Firstname4','LastName4',26,GETDATE(),null)
+USE Class4DB
 
-INSERT INTO dbo.Class VALUES ('Class1',201,GETDATE(),null)
-INSERT INTO dbo.Class VALUES ('Class2',202,GETDATE(),null)
-INSERT INTO dbo.Class VALUES ('Class3',203,GETDATE(),null)
-INSERT INTO dbo.Class VALUES ('Class4',204,GETDATE(),null)
+IF((Select count(1) From dbo.Student)=0)
+BEGIN 
+	INSERT INTO dbo.Student VALUES ('Firstname1','LastName1',23,GETDATE(),null)
+	INSERT INTO dbo.Student VALUES ('Firstname2','LastName2',24,GETDATE(),null)
+	INSERT INTO dbo.Student VALUES ('Firstname3','LastName3',25,GETDATE(),null)
+	INSERT INTO dbo.Student VALUES ('Firstname4','LastName4',26,GETDATE(),null)
+END
 
-INSERT INTO dbo.Student_Class VALUES (1,1,GETDATE(),null)
-INSERT INTO dbo.Student_Class VALUES (1,2,GETDATE(),null)
-INSERT INTO dbo.Student_Class VALUES (1,3,GETDATE(),null)
-INSERT INTO dbo.Student_Class VALUES (2,1,GETDATE(),null)
-INSERT INTO dbo.Student_Class VALUES (2,2,GETDATE(),null)
-INSERT INTO dbo.Student_Class VALUES (2,3,GETDATE(),null)
-INSERT INTO dbo.Student_Class VALUES (3,2,GETDATE(),null)
-INSERT INTO dbo.Student_Class VALUES (3,3,GETDATE(),null)
+IF NOT EXISTS(SELECT 1 FROM Class)
+BEGIN
+	INSERT INTO dbo.Class VALUES ('Class1',201,GETDATE(),null)
+	INSERT INTO dbo.Class VALUES ('Class2',202,GETDATE(),null)
+	INSERT INTO dbo.Class VALUES ('Class3',203,GETDATE(),null)
+	INSERT INTO dbo.Class VALUES ('Class4',204,GETDATE(),null)
+
+	INSERT INTO dbo.Student_Class VALUES (1,1,GETDATE(),null)
+	INSERT INTO dbo.Student_Class VALUES (1,2,GETDATE(),null)
+	INSERT INTO dbo.Student_Class VALUES (1,3,GETDATE(),null)
+	INSERT INTO dbo.Student_Class VALUES (2,1,GETDATE(),null)
+	INSERT INTO dbo.Student_Class VALUES (2,2,GETDATE(),null)
+	INSERT INTO dbo.Student_Class VALUES (2,3,GETDATE(),null)
+	INSERT INTO dbo.Student_Class VALUES (3,2,GETDATE(),null)
+	INSERT INTO dbo.Student_Class VALUES (3,3,GETDATE(),null)
+
+END
