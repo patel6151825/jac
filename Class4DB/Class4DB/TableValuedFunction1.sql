b74dc4ed60b48e8542685c6b0958724458fb0174
Payal Patel <patelpayal1796@gmail.com>
2019-11-28 19:27:24 -0500

    Added one table function

---
+++ Class4DB/Class4DB/TableValuedFunction1.sql
@@ -0,0 +1,11 @@
+﻿CREATE FUNCTION [dbo].[TableValuedFunction1]
+(
+	@StudentId int
+)
+RETURNS TABLE
+AS
+RETURN
+(
+	--SELECT S.StudentId, S.FirstName, S.LastName, S.Age FROM Student S JOIN dbo.Student_Class AS SC ON S.StudentId=SC.StudentId WHERE SC.StudentId=@StudentId
+	SELECT SC.StudentId,SC.ClassId,SC.CreateDate,SC.UpdateDate FROM Student_Class SC WHERE SC.StudentId=@StudentId
+);
